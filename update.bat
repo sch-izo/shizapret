c1ff99800767d3671c1ce91c3350b752844ca16f
sch-izo <gvovagvov@gmail.com>
2025-06-13 17:43:21 +0500

    Add params

--- update.bat
+++ update.bat
@@ -103,66 +103,78 @@ echo 7tv.io>> lists/list-general.txt
 exit /b
 
 :et
-cls
-echo Downloading winws.exe...
-powershell -Command "Start-BitsTransfer -Source https://github.com/bol-van/zapret-win-bundle/raw/refs/heads/master/zapret-winws/winws.exe -Destination bin"
-cls
-echo Downloading WinDivert.dll...
-powershell -Command "Start-BitsTransfer -Source https://github.com/bol-van/zapret-win-bundle/raw/refs/heads/master/zapret-winws/WinDivert.dll -Destination bin"
-cls
-echo Downloading WinDivert64.sys...
-powershell -Command "Start-BitsTransfer -Source https://github.com/bol-van/zapret-win-bundle/raw/refs/heads/master/zapret-winws/WinDivert64.sys -Destination bin"
-cls
-echo Downloading cygwin1.dll...
-powershell -Command "Start-BitsTransfer -Source https://github.com/bol-van/zapret-win-bundle/raw/refs/heads/master/zapret-winws/cygwin1.dll -Destination bin"
-cls
-echo Downloading ipset-cloudflare.txt...
-powershell -Command "Start-BitsTransfer -Source https://raw.githubusercontent.com/V3nilla/IPSets-For-Bypass-in-Russia/refs/heads/main/ipset-cloudflare.txt -Destination lists"
-cls
-echo Downloading list-general.txt...
-powershell -Command "Start-BitsTransfer -Source https://p.thenewone.lol/domains-export.txt -Destination lists/list-general.txt"
-echo cloudflare-ech.com>> lists/list-general.txt
-echo dis.gd>> lists/list-general.txt
-echo discord-attachments-uploads-prd.storage.googleapis.com>> lists/list-general.txt
-echo discord.app>> lists/list-general.txt
-echo discord.co>> lists/list-general.txt
-echo discord.com>> lists/list-general.txt
-echo discord.design>> lists/list-general.txt
-echo discord.dev>> lists/list-general.txt
-echo discord.gift>> lists/list-general.txt
-echo discord.gifts>> lists/list-general.txt
-echo discord.gg>> lists/list-general.txt
-echo discord.media>> lists/list-general.txt
-echo discord.new>> lists/list-general.txt
-echo discord.store>> lists/list-general.txt
-echo discord.status>> lists/list-general.txt
-echo discord-activities.com>> lists/list-general.txt
-echo discordactivities.com>> lists/list-general.txt
-echo discordapp.com>> lists/list-general.txt
-echo discordapp.net>> lists/list-general.txt
-echo discordcdn.com>> lists/list-general.txt
-echo discordmerch.com>> lists/list-general.txt
-echo discordpartygames.com>> lists/list-general.txt
-echo discordsays.com>> lists/list-general.txt
-echo discordsez.com>> lists/list-general.txt
-echo ggpht.com>> lists/list-general.txt
-echo googlevideo.com>> lists/list-general.txt
-echo jnn-pa.googleapis.com>> lists/list-general.txt
-echo stable.dl2.discordapp.net>> lists/list-general.txt
-echo wide-youtube.l.google.com>> lists/list-general.txt
-echo youtube-nocookie.com>> lists/list-general.txt
-echo youtube-ui.l.google.com>> lists/list-general.txt
-echo youtube.com>> lists/list-general.txt
-echo youtubeembeddedplayer.googleapis.com>> lists/list-general.txt
-echo youtubekids.com>> lists/list-general.txt
-echo youtubei.googleapis.com>> lists/list-general.txt
-echo youtu.be>> lists/list-general.txt
-echo yt-video-upload.l.google.com>> lists/list-general.txt
-echo ytimg.com>> lists/list-general.txt
-echo ytimg.l.google.com>> lists/list-general.txt
-echo frankerfacez.com>> lists/list-general.txt
-echo ffzap.com>> lists/list-general.txt
-echo betterttv.net>> lists/list-general.txt
-echo 7tv.app>> lists/list-general.txt
-echo 7tv.io>> lists/list-general.txt
+if exist "params/Updater/EverythingWinws1" (
+    cls
+    echo Downloading winws.exe...
+    powershell -Command "Start-BitsTransfer -Source https://github.com/bol-van/zapret-win-bundle/raw/refs/heads/master/zapret-winws/winws.exe -Destination bin"
+)
+if exist "params/Updater/EverythingWinDivert1" (
+    cls
+    echo Downloading WinDivert.dll...
+    powershell -Command "Start-BitsTransfer -Source https://github.com/bol-van/zapret-win-bundle/raw/refs/heads/master/zapret-winws/WinDivert.dll -Destination bin"
+)
+if exist "params/Updater/EverythingWinDivert641" (
+    cls
+    echo Downloading WinDivert64.sys...
+    powershell -Command "Start-BitsTransfer -Source https://github.com/bol-van/zapret-win-bundle/raw/refs/heads/master/zapret-winws/WinDivert64.sys -Destination bin"
+)
+if exist "params/Updater/EverythingCygwin11" (
+    cls
+    echo Downloading cygwin1.dll...
+    powershell -Command "Start-BitsTransfer -Source https://github.com/bol-van/zapret-win-bundle/raw/refs/heads/master/zapret-winws/cygwin1.dll -Destination bin"
+)
+if exist "params/Updater/EverythingIPSet1" (
+    cls
+    echo Downloading ipset-cloudflare.txt...
+    powershell -Command "Start-BitsTransfer -Source https://raw.githubusercontent.com/V3nilla/IPSets-For-Bypass-in-Russia/refs/heads/main/ipset-cloudflare.txt -Destination lists"
+)
+if exist "params/Updater/EverythingList1" (
+    cls
+    echo Downloading list-general.txt...
+    powershell -Command "Start-BitsTransfer -Source https://p.thenewone.lol/domains-export.txt -Destination lists/list-general.txt"
+    echo cloudflare-ech.com>> lists/list-general.txt
+    echo dis.gd>> lists/list-general.txt
+    echo discord-attachments-uploads-prd.storage.googleapis.com>> lists/list-general.txt
+    echo discord.app>> lists/list-general.txt
+    echo discord.co>> lists/list-general.txt
+    echo discord.com>> lists/list-general.txt
+    echo discord.design>> lists/list-general.txt
+    echo discord.dev>> lists/list-general.txt
+    echo discord.gift>> lists/list-general.txt
+    echo discord.gifts>> lists/list-general.txt
+    echo discord.gg>> lists/list-general.txt
+    echo discord.media>> lists/list-general.txt
+    echo discord.new>> lists/list-general.txt
+    echo discord.store>> lists/list-general.txt
+    echo discord.status>> lists/list-general.txt
+    echo discord-activities.com>> lists/list-general.txt
+    echo discordactivities.com>> lists/list-general.txt
+    echo discordapp.com>> lists/list-general.txt
+    echo discordapp.net>> lists/list-general.txt
+    echo discordcdn.com>> lists/list-general.txt
+    echo discordmerch.com>> lists/list-general.txt
+    echo discordpartygames.com>> lists/list-general.txt
+    echo discordsays.com>> lists/list-general.txt
+    echo discordsez.com>> lists/list-general.txt
+    echo ggpht.com>> lists/list-general.txt
+    echo googlevideo.com>> lists/list-general.txt
+    echo jnn-pa.googleapis.com>> lists/list-general.txt
+    echo stable.dl2.discordapp.net>> lists/list-general.txt
+    echo wide-youtube.l.google.com>> lists/list-general.txt
+    echo youtube-nocookie.com>> lists/list-general.txt
+    echo youtube-ui.l.google.com>> lists/list-general.txt
+    echo youtube.com>> lists/list-general.txt
+    echo youtubeembeddedplayer.googleapis.com>> lists/list-general.txt
+    echo youtubekids.com>> lists/list-general.txt
+    echo youtubei.googleapis.com>> lists/list-general.txt
+    echo youtu.be>> lists/list-general.txt
+    echo yt-video-upload.l.google.com>> lists/list-general.txt
+    echo ytimg.com>> lists/list-general.txt
+    echo ytimg.l.google.com>> lists/list-general.txt
+    echo frankerfacez.com>> lists/list-general.txt
+    echo ffzap.com>> lists/list-general.txt
+    echo betterttv.net>> lists/list-general.txt
+    echo 7tv.app>> lists/list-general.txt
+    echo 7tv.io>> lists/list-general.txt
+)
 exit /b
